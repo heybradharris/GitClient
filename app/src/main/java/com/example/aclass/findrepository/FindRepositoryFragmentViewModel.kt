a723f627ac825246d1f9b6e8aec41951347c1749
Brad Harris <heybradharris@gmail.com>
2021-08-13 21:13:09 -0700

    adds theme colors

--- app/src/main/java/com/example/aclass/findrepository/FindRepositoryFragmentViewModel.kt
+++ app/src/main/java/com/example/aclass/findrepository/FindRepositoryFragmentViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.aclass.findrepository
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class FindRepositoryFragmentViewModel : ViewModel() {
+@HiltViewModel
+class FindRepositoryFragmentViewModel @Inject constructor() : ViewModel() {
 }
