c7ee53ce77bb87dd61442ab2142b8b8c6e2b16cb
Brad Harris <heybradharris@gmail.com>
2021-08-22 08:20:32 -0700

    adds single file view for new and deleted diff sections

--- app/src/main/java/com/example/aclass/diff/viewholder/DiffViewHolder.kt
+++ app/src/main/java/com/example/aclass/diff/viewholder/DiffViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.aclass.diff.viewholder
 
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.example.aclass.databinding.ItemDiffBinding
 import com.example.aclass.diff.DiffRecyclerItem
@@ -21,6 +22,10 @@ class DiffViewHolder(
     }
 
     fun bind(diffItem: DiffRecyclerItem.Diff) {
+        binding.firstCommitContainer.isVisible = true
+        binding.divider.isVisible = true
+        binding.secondCommitContainer.isVisible = true
+
         binding.firstCommitScrollView.scrollTo(0, 0)
         binding.firstCommit.text = diffItem.firstCommitLines
         binding.secondCommitScrollView.scrollTo(0, 0)
@@ -28,6 +33,34 @@ class DiffViewHolder(
         binding.firstCommitLineNumbers.text = diffItem.firstCommitLineNumbers
         binding.secondCommitLineNumbers.text = diffItem.secondCommitLineNumbers
 
-        // Hide side if blank
+        // if first or second commit lines is only new lines, then hide commit container
+        // only necessary to show the new or deleted file, not sharing a side
+        var isHideLeft = true
+
+        for (char in diffItem.firstCommitLines) {
+            if (char != '\n') {
+                isHideLeft = false
+                break;
+            }
+        }
+
+        if (isHideLeft) {
+            binding.firstCommitContainer.isVisible = false
+            binding.divider.isVisible = false
+        }
+
+        var isHideRight = true
+
+        for (char in diffItem.secondCommitLines) {
+            if (char != '\n') {
+                isHideRight = false
+                break;
+            }
+        }
+
+        if (isHideRight) {
+            binding.secondCommitContainer.isVisible = false
+            binding.divider.isVisible = false
+        }
     }
 }
