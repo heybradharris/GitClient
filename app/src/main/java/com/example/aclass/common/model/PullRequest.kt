451ac95d2baa5d05979091a3af615319f51e0748
Brad <brad@act4software.com>
2025-01-12 17:06:47 -0800

    fixes JsonDataException

--- app/src/main/java/com/example/aclass/common/model/PullRequest.kt
+++ app/src/main/java/com/example/aclass/common/model/PullRequest.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class PullRequest(
-    @Json(name = "id") val id: Int,
+    @Json(name = "id") val id: Long,
     @Json(name = "diff_url") val diffUrl: String,
     @Json(name = "number") val number: Int,
     @Json(name = "state") val state: String,
